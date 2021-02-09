b94d071c71035625038e7b2dfd47cc7f99c7431f
Morgan Bradford <morganjbradford95@gmail.com>
2021-02-09 13:50:37 -0600

    add dinoipsum-service/ui logic
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,4 +2,19 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import './css/styles.css';
+import {DinoIpsum} from './js/dinoipsum-service.js'
+
+
+$(document).ready(function() {
+  $("#dinoButton").click(function() {
+    //clearFields();
+    let promise = DinoIpsum.getDinoIpsum();
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      let wordArray = body[0][0].split();
+      $("#")
+    }, function(error) {
+      $(".showErrors").text(`ERROR!`)
+    });
+  });
+});
